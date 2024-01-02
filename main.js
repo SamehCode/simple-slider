69dd7e55f7a168e6948905182b922858d6e63045
SamehCode <jack_jones2010@hotmail.com>
2024-01-02 07:31:08 +0200

    uploaded main project files

---
+++ main.js
@@ -0,0 +1,64 @@
+let sliderImages = document.getElementById('slider');
+let prevBtn = document.getElementById('prev');
+let nextBtn = document.getElementById('next');
+let bulletsContainer = document.getElementById('bullets');
+let allImages = ['img1', 'img2', 'img3', 'img4', 'img5', 'img6'];
+let currentslide = 1;
+let allSlides = document.querySelectorAll('.slider-imgs .image'),
+    allSlidesImgs = document.querySelectorAll('.slider-imgs .image img'),
+    backImg = document.querySelector('.back-img');
+let bullet,
+    allBullets = bulletsContainer.children;
+
+allImages.forEach((img, idx) => {
+    bullet = `<li class='bullet' data-num='${idx + 1}'>${idx + 1}</li>`;
+    bulletsContainer.innerHTML += (bullet)
+})
+
+bulletsContainer.children[0].classList.add('active')
+
+prevBtn.onclick = prevSlide;
+nextBtn.onclick = nextSlide;
+
+//prev btn logic
+
+function prevSlide () {
+    currentslide--
+    console.log(currentslide)
+    if(currentslide == '0') {
+        prevBtn.classList.add('disabled')
+        currentslide = allImages.length + 1
+        // return false
+    }else {
+        prevBtn.classList.remove('disabled')
+        allSlides.forEach(slide => slide.classList.remove('active'));
+        allSlides[currentslide - 1].classList.add('active')
+        allSlidesImgs.forEach(img => img.classList.remove('active'));
+        allSlidesImgs[currentslide - 1].classList.add('active')
+        backImg.style.backgroundImage = 'url(' + './imgs/' + allImages[currentslide - 1] + '.jpg)';
+        document.querySelectorAll('.bullet').forEach(bullet => bullet.classList.remove('active'))
+        document.querySelectorAll('.bullet')[currentslide - 1].classList.add('active')
+    }
+}
+
+function nextSlide () {
+    currentslide++
+    if(currentslide > allImages.length) {
+        nextBtn.classList.add('disabled')
+        currentslide = 0
+        return false
+    }else {
+        nextBtn.classList.remove('disabled')
+        allSlides.forEach(slide => slide.classList.remove('active'));
+        allSlides[currentslide - 1].classList.add('active')
+        allSlidesImgs.forEach(img => img.classList.remove('active'));
+        allSlidesImgs[currentslide - 1].classList.add('active')
+        backImg.style.backgroundImage = 'url(' + './imgs/' + allImages[currentslide - 1] + '.jpg)';
+        document.querySelectorAll('.bullet').forEach(bullet => bullet.classList.remove('active'))
+        document.querySelectorAll('.bullet')[currentslide - 1].classList.add('active')
+
+    }
+}
+
+
+// console.log(backImg.style.backgroundImage = `url(./imgs/img2.jpg)`);
